958f295d2e06b2595ce67560d7a3fea64c000eb8
wangxiang <wangxiangc@gamil.com>
2016-11-21 16:14:52 +0800

    增加 hello world

--- readout.js
+++ readout.js
@@ -3,7 +3,8 @@ var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/readtimeout', function(req, res) {
-  res.send('hello world');
+	console.log('get request, not response');
+  //res.send('hello world');
 });
 
 app.listen(3000, function () {
